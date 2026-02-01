7d2bff6b4d6ab311b5e497ab0d967f0aea8b2351
René Ruts <rene.ruts@student.howest.be>
2026-02-01 23:07:53 +0100

    show single idea

---
+++ resources/views/components/icons/arrow-back.php
@@ -0,0 +1,4 @@
+<svg widht="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" role="img" aria-label="Arrow Back">
+  <line x1="19" y1="12" x2="5" y2="12" />
+  <polyline points="12 19 5 12 12 5" />
+</svg>
---
+++ resources/views/components/icons/external.blade.php
@@ -0,0 +1,6 @@
+<svg widht="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+    stroke-linecap="round" stroke-linejoin="round" role="img" aria-label="External">
+    <path d="M14 4H20M20 4V10M20 4L12 12" stroke="#33363F" stroke-width="2" />
+    <path d="M11 5H7C5.89543 5 5 5.89543 5 7V17C5 18.1046 5.89543 19 7 19H17C18.1046 19 19 18.1046 19 17V13"
+        stroke="#33363F" stroke-width="2" stroke-linecap="round" />
+</svg>
